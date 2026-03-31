f272d7c0f03e522749079270ec28ee4f7b39d5f1
Łukasz Sarzyński <44730987+LukaszSarzynski@users.noreply.github.com>
2026-03-31 19:49:41 +0200

    landing list

--- frontend/src/app/pages/landing-sites/landing-sites.ts
+++ frontend/src/app/pages/landing-sites/landing-sites.ts
@@ -1,17 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ViewWillEnter } from '@ionic/angular';
+import { RouterLink } from '@angular/router';
 import {
+  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
+  IonItem,
+  IonLabel,
+  IonList,
   IonMenuButton,
+  IonNote,
+  IonProgressBar,
   IonTitle,
   IonToolbar,
 } from '@ionic/angular/standalone';
 
+import { AirfieldApiService } from '../../services/airfield-api.service';
+import { AirfieldResponseDto } from '../../models/airfield.model';
+import { getErrorMessage } from '../../shared/utils/error-messages';
+
 @Component({
   selector: 'app-landing-sites',
-  imports: [IonButtons, IonContent, IonHeader, IonMenuButton, IonTitle, IonToolbar],
+  imports: [
+    RouterLink,
+    IonButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonMenuButton,
+    IonNote,
+    IonProgressBar,
+    IonTitle,
+    IonToolbar,
+  ],
   templateUrl: './landing-sites.html',
   styleUrl: './landing-sites.scss',
 })
-export class LandingSitesPage {}
+export class LandingSitesPage implements ViewWillEnter {
+  private airfieldApi = inject(AirfieldApiService);
+
+  airfields = signal<AirfieldResponseDto[]>([]);
+  loading = signal(true);
+  errorMessage = signal('');
+
+  ionViewWillEnter(): void {
+    this.loadAirfields();
+  }
+
+  private loadAirfields(): void {
+    this.loading.set(true);
+    this.errorMessage.set('');
+
+    this.airfieldApi.getAll().subscribe({
+      next: (data) => {
+        this.airfields.set(data.sort((a, b) => a.name.localeCompare(b.name)));
+        this.loading.set(false);
+      },
+      error: (err) => {
+        this.errorMessage.set(getErrorMessage(err, 'Nie udało się pobrać listy lądowisk.'));
+        this.loading.set(false);
+      },
+    });
+  }
+}
