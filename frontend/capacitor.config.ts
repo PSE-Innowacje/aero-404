607a56284345f16520db75bc7499c46485e07609
Łukasz Sarzyński <44730987+LukaszSarzynski@users.noreply.github.com>
2026-04-01 09:24:21 +0200

    ionic config

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'team.not.found',
+  appName: 'areo',
+  webDir: 'dist/frontend/browser'
+};
+
+export default config;
